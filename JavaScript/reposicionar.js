6b6e53b3958b786167be6e7fa04fe105bcbab124
Sebastian Junco <sebastianj.desarrollo@gmail.com>
2020-12-12 17:30:19 -0500

    Se creo la logica en donde se identifica la cantidad de usuarios que existen en la base de datos sin importar si se visualicen en la tabla de administracion o no

--- JavaScript/reposicionar.js
+++ JavaScript/reposicionar.js
@@ -1,2 +1,23 @@
 'use strict'
 
+function cantidadUsuarios(){
+    const tamLocalStorage = localStorage.length;
+    const cantUsuarios = Math.round(tamLocalStorage/7)+1;
+
+    var contador = 0;
+    var usuario = "";
+    do{
+
+        contador++;
+        if(usuario != void 0 && usuario != ""){
+            var nuevo = usuario
+        }
+        usuario = localStorage.getItem("Nombre" + contador);
+        
+    }while(contador <= cantUsuarios)
+
+    console.log(nuevo);
+
+}
+
+cantidadUsuarios();
